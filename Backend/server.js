b67ddd8733e0a94a5f30659c5f19abb34ec0ebf5
Sebastian Gatti <juanse10028@gmail.com>
2025-12-27 23:45:18 -0300

    solving some issues

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "*",
+  })
+);
 app.use(express.json());
 
 // Rutas de la API
