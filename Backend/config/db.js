e821b8777db9d658d1b8e94df441896915c6b1c6
Sebastian Gatti <juanse10028@gmail.com>
2025-12-28 14:12:07 -0300

    solving issues with backend deply

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -7,7 +7,15 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 4000, // TiDB usa puerto 4000
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  // 👇 ESTO ES LO QUE TE FALTA 👇
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
